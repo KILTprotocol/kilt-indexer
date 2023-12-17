ae24816e1f4ca36a8ba77e3fb141b7024274157b
kilted-andres <andres@trusted-entity.io>
2023-12-17 15:31:09 +0100

    fix: rephrase projects description

--- project.ts
+++ project.ts
@@ -12,7 +12,7 @@ const project: SubstrateProject = {
   version: "0.0.1",
   name: CRAWL_PEREGRINE ? "kilt-peregrine-indexer" : "kilt-spiritnet-indexer",
   description:
-    "This project crawls the KILT network and registers all claim types and credential attestations; with custom aggregations.",
+    "This projects registers (with custom aggregations) all claim types and credential attestations from the KILT network.",
   runner: {
     node: {
       name: "@subql/node",
