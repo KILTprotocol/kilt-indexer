499999197253c64a6dd9b866b3131bc2ddba9e56
kilted-andres <andres@trusted-entity.io>
2024-01-25 11:16:53 +0100

    fix: squashed merge xw--did-entity

--- configuration.ts
+++ configuration.ts
@@ -8,17 +8,10 @@ export const CRAWL_PEREGRINE = loadEnv("CRAWL_PEREGRINE", false);
 export const PRIVATE_NODE = loadEnv("PRIVATE_NODE", false);
 
 function loadEnv(name: string, compulsory: boolean = true) {
-function loadEnv(name: string, compulsory: boolean = true) {
   const envValue = process.env[name];
-  if (!envValue && compulsory) {
   if (!envValue && compulsory) {
     throw new Error(
       `Environment constant '${name}' is missing. Define it on the project's root directory '.env'-file. \n`
-    );
-  }
-  if (!envValue) {
-    console.log(
-      `Environment constant '${name}' is missing from '.env'-file. Working with default fallback.  \n`
     );
   }
   if (!envValue) {
