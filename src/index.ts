9531e886ba12d0e59ec12e1c570e43dcae1b9ad8
kilted-andres <andres@trusted-entity.io>
2024-03-19 22:44:21 +0100

    fix: import order

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+import { TextDecoder, TextEncoder } from "util";
+// make available some modules that the `cborg`-module depends on
+(global as any).TextDecoder = TextDecoder;
+(global as any).TextEncoder = TextEncoder;
+
 //Exports all handler functions
 export * from "./mappings/mappingHandlers";
 import "@polkadot/api-augment";
-
-import { TextDecoder, TextEncoder } from "util";
-
-(global as any).TextDecoder = TextDecoder;
-(global as any).TextEncoder = TextEncoder;
