1c004877e9fde250c3133b7ace558e03771267d4
kilted-andres <andres@trusted-entity.io>
2024-04-19 17:52:31 +0200

    chore: exits debug mode
    This reverts commit 5e97763c2a7d8278d32bcbc86a03e9be58473f89.

--- src/mappings/assetDIDs/extractCredential.ts
+++ src/mappings/assetDIDs/extractCredential.ts
@@ -46,7 +46,7 @@ export function extractCredential(
 
   const usedCall: GenericExtrinsic["method"] = extrinsic.extrinsic.method;
 
-  logger.info("The whole extrinsic: " + JSON.stringify(usedCall, null, 2));
+  logger.trace("The whole extrinsic: " + JSON.stringify(usedCall, null, 2));
 
   let credential: CredentialFromChain | false;
 
@@ -428,7 +428,7 @@ function validateCredentialAgainstHash(
   attesterDidAccount: Codec,
   targetCredentialHash: HexString
 ): CredentialFromChain | false {
-  logger.info(
+  logger.trace(
     "The target CredentialHash from the event: " + targetCredentialHash
   );
 
@@ -438,7 +438,7 @@ function validateCredentialAgainstHash(
     Uint8Array.from([...encodedCredential, ...encodedAttester])
   );
 
-  logger.info("The resulting Credential ID is: " + hashedCredential);
+  logger.trace("The resulting Credential ID is: " + hashedCredential);
 
   const attesterDid = ("did:kilt:" + attesterDidAccount) as DidUri;
 
