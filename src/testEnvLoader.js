7e17a667e2f90ec47f07fe8c9d4a3c334b36b07c
kilted-andres <andres@kilt.io>
2025-02-26 17:22:27 +0100

    fix: delete commented code

--- src/testEnvLoader.js
+++ src/testEnvLoader.js
@@ -1,21 +1,5 @@
 const dotenv = require("dotenv");
 
-// dotenv.config();
-
-// function loadEnvironmentForTest() {
-//   console.log("loading test environment...");
-
-//   const TZ = "UTC";
-//   const DB_PORT = process.env.POSTGRES_HOST_PORT;
-
-//   console.log(`print the db_port=${DB_PORT} `);
-//   return { TZ, DB_PORT };
-// }
-
-// const testEnv = loadEnvironmentForTest();
-
-// module.exports = testEnv;
-
 module.exports = new Promise((resolve) => {
   dotenv.config();
 
@@ -34,10 +18,3 @@ module.exports = new Promise((resolve) => {
     resolve(testEnv);
   }, 200);
 });
-
-// // env.js
-// module.exports = Promise.resolve({
-//   DB_HOST: "localhost",
-//   DB_PORT: 7777,
-//   NODE_ENV: "development",
-// });
