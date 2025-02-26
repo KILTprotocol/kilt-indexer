abb39810aa1383792b29842bbe27238b710ede86
kilted-andres <andres@kilt.io>
2025-02-26 17:26:33 +0100

    fix: reduce logs

--- src/testEnvLoader.js
+++ src/testEnvLoader.js
@@ -3,15 +3,15 @@ const dotenv = require("dotenv");
 module.exports = new Promise((resolve) => {
   dotenv.config();
 
-  console.log("loading test environment...");
-
   const testEnv = {
     TZ: "UTC",
     DB_PORT: process.env.POSTGRES_HOST_PORT,
   };
 
   console.log(
-    `The test env that we are trying to pass: ${JSON.stringify(testEnv)}`
+    `Passing this environment variables fot the tests: ${JSON.stringify(
+      testEnv
+    )}`
   );
 
   setTimeout(() => {
